2df708920fa06fadfaf2c5c4b53bdfd48763ec7e
Christine Cua <cloudycua@gmail.com>
2019-03-06 12:36:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,4 @@ function retrieveEmployeeInformation() {
 function addNewElementAsLi() {
   let employeeName = retrieveEmployeeInformation()
   document.querySelector('.employee-list').insertAdjacentHTML('beforeend', `<li>${employeeName}</li>`)
+}
